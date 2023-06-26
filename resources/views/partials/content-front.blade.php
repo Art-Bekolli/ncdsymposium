2a3560053625d61864351c687d130e251ec7471c
Art-Bekolli <abekolli57@gmail.com>
2023-06-26 11:17:48 +0200

    blue

--- resources/views/partials/content-front.blade.php
+++ resources/views/partials/content-front.blade.php
@@ -7,7 +7,11 @@
             <div class="three">@field("hero_text3")</div>
         </div>
 
-        <div class="right"> <img src="@field('hero_img')" alt="" class="hero-img"></div>
+        <div class="right">
+            <img src="@field('hero_img')" alt="" class="hero-img">
+            <img src="@field('hero_img2')" alt="" class="hero-img2">
+            <img src="@field('hero_img3')" alt="" class="hero-img3">
+        </div>
     </div>
 
 </section>
