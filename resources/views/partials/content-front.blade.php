a0bd90f0bdd788a00fdd60806af48a254a0bcd1e
Art-Bekolli <abekolli57@gmail.com>
2023-06-26 14:03:02 +0200

    gati

--- resources/views/partials/content-front.blade.php
+++ resources/views/partials/content-front.blade.php
@@ -15,3 +15,71 @@
     </div>
 
 </section>
+<section class="section__projects">
+
+<div class="container">
+<div class="box">
+    <div class="left">@field('projects_txt')</div>
+    <div class="right">
+        <img src="@field('projects_image_txt')" alt="" class="projects_img">
+    </div>
+</div>
+
+<div class="projects">
+
+    <img src="@field('projects_img1')" alt="" class="project1">
+    <img src="@field('projects_img2')" alt="" class="project2">
+    <img src="@field('projects_img3')" alt="" class="project3">
+    <img src="@field('projects_im4')" alt="" class="project4">
+    <img src="@field('projects_img5')" alt="" class="project5">
+
+</div>
+
+<div class="textarea">
+
+    @field('textarea')
+
+</div>
+</div>
+
+</section>
+<section class="section__speeches">
+
+    <div class="container">
+
+        <div class="welcome">
+            @field('welcome_speech_txt')
+            <div class="speakers">
+                <div class="speakers__one"> <img src="@field('welcome_one')" alt="" class="speaker-img"> <div class="text"> <div class="name">Dr. Arben Vitia</div>  <div class="title">Ministria of Health</div></div></div>
+                <div class="speakers__two"><img src="@field('welcome_two')" alt="" class="speaker-img"> <div class="text"> <div class="name">Dr. Naser Ramadani</div>  <div class="title">NIPHK</div></div></div>
+                <div class="speakers__three"><img src="@field('welcome_three')" alt="" class="speaker-img"> <div class="text"> <div class="name">Mr. Laurent Torche</div>  <div class="title">SDC - Swiss Embassy</div></div></div>
+            </div>
+        </div>
+
+        <div class="keynote">
+            @field('keynote_speech_txt')
+            <div class="speakers">
+                <div class="speakers__one"><img src="@field('welcome_three')" alt="" class="speaker-img"> <div class="text"> <div class="name">Mr. Sean Taylor</div>  <div class="title">MS, World Heart Federation</div></div></div>
+                <div class="speakers__two"><img src="@field('welcome_three')" alt="" class="speaker-img"> <div class="text"> <div class="name">Dr. Fatmire Mulhaxha- Kollcaku</div>  <div class="title">Chairman of the Commission for<br> health and Social Welfare</div></div></div>
+                <div class="speakers__three"><img src="@field('welcome_three')" alt="" class="speaker-img"> <div class="text"> <div class="name">Dr. Rushit ismaili</div>  <div class="title">Family Physician</div></div></div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="section__paragraph">
+
+<div class="container">
+
+    <div class="paragraph">@field('paragraph2')</div>
+    <div class="p-img"> <svg width="257" height="269" viewBox="0 0 257 269" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2.34626 93.5102L128.5 1.85411L254.654 93.5102L206.467 241.813H50.5327L2.34626 93.5102Z" stroke="#1FE0E0" stroke-width="3"/>
+        <path d="M2.65064 176.415L49.747 27.7626L205.677 26.6177L254.951 174.563L129.474 267.142L2.65064 176.415Z" stroke="#2A5DE0" stroke-width="3"/>
+        </svg>
+        </div>
+
+</div>
+
+</section>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,20 @@
 <footer class="content-info">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="left">
+
+      <div class="press">Press inquiries:</div>
+
+      <div class="info1">
+        AQH Senior Communication<br>
+        zana.aqifi@aqhproject.org
+      </div>
+
+      <div class="info2">
+        IHS Advocacy and Communication<br>
+        linda.baleta@ihsproject.org
+      </div>
+
+    </div>
+    <div class="right"><span class="more">More information will follow soon.</span></div>
   </div>
 </footer>
