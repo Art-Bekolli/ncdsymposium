34c6e4a95fb9c634b9a434a7b78aac7e4bacbec5
Art-Bekolli <abekolli57@gmail.com>
2023-07-05 00:40:03 +0200

    test

--- resources/views/partials/components/lang-switch.blade.php
+++ resources/views/partials/components/lang-switch.blade.php
@@ -1,5 +1,5 @@
 <div class="language-button">
-<form method="POST" action="">
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <select name="language" onchange="this.form.submit()">
         <option @php if ($_SESSION['lang_v'] == '1') echo ' selected="selected"'; @endphp value="1">English</option>
         <option @php if ($_SESSION['lang_v'] == '2') echo ' selected="selected"'; @endphp value="2">Shqip</option>
