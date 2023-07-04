93a197518e78df077d293d548c360a2d86ff38d5
Art-Bekolli <abekolli57@gmail.com>
2023-07-04 15:45:19 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,20 +1,26 @@
+@php
+
+if(isset($_POST["language"])){
+       $langf=$_POST["language"];
+}
+@endphp
 <footer class="content-info">
   <div class="container">
     <div class="left">
 
-      <div class="press"> @php echo get_field('press_txt' . $_SESSION['lang'], 279); @endphp</div>
+      <div class="press"> @php echo get_field('press_txt' . $langf, 279); @endphp</div>
 
       <div class="info1">
-        @php echo get_field('press1_pos' . $_SESSION['lang'], 279); @endphp<br>
-        @php echo get_field('press1_name' . $_SESSION['lang'], 279); @endphp
+        @php echo get_field('press1_pos' . $langf, 279); @endphp<br>
+        @php echo get_field('press1_name' . $langf, 279); @endphp
       </div>
 
       <div class="info2">
-        @php echo get_field('press2_pos' . $_SESSION['lang'], 279); @endphp<br>
-        @php echo get_field('press2_name' . $_SESSION['lang'], 279); @endphp
+        @php echo get_field('press2_pos' . $langf, 279); @endphp<br>
+        @php echo get_field('press2_name' . $langf, 279); @endphp
       </div>
 
     </div>
-      <div class="right"><div class="more">@php echo get_field('information_txt' . $_SESSION['lang'], 279); @endphp</div></div>
+      <div class="right"><div class="more">@php echo get_field('information_txt' . $langf, 279); @endphp</div></div>
   </div>
 </footer>
