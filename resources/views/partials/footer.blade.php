e4bc0778c306313e45ca120daf79f5812ac4bbed
Art-Bekolli <abekolli57@gmail.com>
2023-07-04 15:06:18 +0200

    text

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,19 +2,19 @@
   <div class="container">
     <div class="left">
 
-      <div class="press">Press inquiries:</div>
+      <div class="press">@field('press_txt' . $_SESSION['lang'])</div>
 
       <div class="info1">
-        AQH<br>
-        zana.aqifi@aqhproject.org
+        @field('press1_pos' . $_SESSION['lang'])<br>
+        @field('press1_name' . $_SESSION['lang'])
       </div>
 
       <div class="info2">
-        IHS<br>
-        linda.baleta@ihsproject.org
+        @field('press2_pos' . $_SESSION['lang'])<br>
+        @field('press2_name' . $_SESSION['lang'])
       </div>
 
     </div>
-    <div class="right"><div class="more">More information will follow soon.</div></div>
+    <div class="right"><div class="more">@field('information_txt' . $_SESSION['lang'])</div></div>
   </div>
 </footer>
