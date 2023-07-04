71b26f36317f7902582a2cc8d14e69a773b802ce
Art-Bekolli <abekolli57@gmail.com>
2023-07-04 15:17:33 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,19 +2,19 @@
   <div class="container">
     <div class="left">
 
-      <div class="press">@field('press_txt' . $_SESSION['lang'] , 279)</div>
+      <div class="press"> @php echo get_field('press_txt' . $_SESSION['lang'], 279); @endphp</div>
 
       <div class="info1">
-        @field('press1_pos' . $_SESSION['lang'] , 279)<br>
-        @field('press1_name' . $_SESSION['lang'] , 279)
+        @php echo get_field('press1_pos' . $_SESSION['lang'], 279); @endphp<br>
+        @php echo get_field('press1_name' . $_SESSION['lang'], 279); @endphp
       </div>
 
       <div class="info2">
-        @field('press2_pos' . $_SESSION['lang'] , 279)<br>
-        @field('press2_name' . $_SESSION['lang'] , 279)
+        @php echo get_field('press2_pos' . $_SESSION['lang'], 279); @endphp<br>
+        @php echo get_field('press2_name' . $_SESSION['lang'], 279); @endphp
       </div>
 
     </div>
-    <div class="right"><div class="more">@field('information_txt' . $_SESSION['lang'] , 279)</div></div>
+    <div class="right"><div class="more">@php echo get_field('information_txt' . $_SESSION['lang'], 279); @endphp</div></div>
   </div>
 </footer>
