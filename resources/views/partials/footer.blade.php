5fc107cce701fcf534798658275dd74af4146f6d
Art-Bekolli <abekolli57@gmail.com>
2023-07-04 15:07:35 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,19 +2,19 @@
   <div class="container">
     <div class="left">
 
-      <div class="press">@field('press_txt' . $_SESSION['lang'])</div>
+      <div class="press">@field('press_txt' . $_SESSION['lang'] , 279)</div>
 
       <div class="info1">
-        @field('press1_pos' . $_SESSION['lang'])<br>
-        @field('press1_name' . $_SESSION['lang'])
+        @field('press1_pos' . $_SESSION['lang'] , 279)<br>
+        @field('press1_name' . $_SESSION['lang'] , 279)
       </div>
 
       <div class="info2">
-        @field('press2_pos' . $_SESSION['lang'])<br>
-        @field('press2_name' . $_SESSION['lang'])
+        @field('press2_pos' . $_SESSION['lang'] , 279)<br>
+        @field('press2_name' . $_SESSION['lang'] , 279)
       </div>
 
     </div>
-    <div class="right"><div class="more">@field('information_txt' . $_SESSION['lang'])</div></div>
+    <div class="right"><div class="more">@field('information_txt' . $_SESSION['lang'] , 279)</div></div>
   </div>
 </footer>
